d3e576e6ec96a386df5d3af534cf3397baf1762e
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-30 20:01:21 +0900

    プロジェクト ファイルを追加します。

---
+++ MainPage.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports LiveCharts
+Imports LiveCharts.Uwp
+' 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+''' <summary>
+''' それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は、以下の属性セットによって制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("LiveChartsUwpVb")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("LiveChartsUwpVb")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      リビジョン
+'
+' すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+' 既定値にすることができます:
+' <アセンブリ: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: ComVisible(False)>
